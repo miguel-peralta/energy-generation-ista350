53447c998060503b81db9ddb6f3b74905f8f3be6
miguel-peralta <115048112+miguel-peralta@users.noreply.github.com>
2023-04-30 13:29:23 -0700

    Figure 3 visualization
    - Finished figure 3 interactive choropleth map
    - Changed figure 3 dataset to be mainland US states only
    
    Other:
    - Changed width of figure 2 to fit legend correctly

--- visualizations.py
+++ visualizations.py
@@ -12,6 +12,8 @@ import json
 import matplotlib.pyplot as plt
 import numpy as np
 import geopandas as gpd
+from matplotlib.widgets import RadioButtons
+from matplotlib.gridspec import GridSpec
 
 #SECTION 1: Methods retrieving data from EIA API
 """
@@ -83,10 +85,10 @@ def get_netgen_by_state(key):
     """
     Given an EIA API key, returns a dataframe. Dataframe contains the net
     generation of electricity separated by generation method (solar, wind,
-    etc.), and US state + District of Columbia.
+    etc.), and mainland US state + District of Columbia.
     """
     print("Getting data from API...")
-    url = f"https://api.eia.gov/v2/electricity/electric-power-operational-data/data/?api_key={key}&frequency=annual&data[0]=generation&start=2021&end=2022&facets[fueltypeid][]=BIO&facets[fueltypeid][]=COW&facets[fueltypeid][]=GEO&facets[fueltypeid][]=HYC&facets[fueltypeid][]=NG&facets[fueltypeid][]=NUC&facets[fueltypeid][]=PC&facets[fueltypeid][]=PEL&facets[fueltypeid][]=SUN&facets[fueltypeid][]=WND&facets[location][]=AK&facets[location][]=AL&facets[location][]=AR&facets[location][]=AZ&facets[location][]=CA&facets[location][]=CO&facets[location][]=CT&facets[location][]=DC&facets[location][]=DE&facets[location][]=FL&facets[location][]=GA&facets[location][]=HI&facets[location][]=IA&facets[location][]=ID&facets[location][]=IL&facets[location][]=IN&facets[location][]=KS&facets[location][]=KY&facets[location][]=LA&facets[location][]=MA&facets[location][]=MD&facets[location][]=ME&facets[location][]=MI&facets[location][]=MN&facets[location][]=MO&facets[location][]=MS&facets[location][]=MT&facets[location][]=NC&facets[location][]=ND&facets[location][]=NE&facets[location][]=NH&facets[location][]=NJ&facets[location][]=NM&facets[location][]=NV&facets[location][]=NY&facets[location][]=OH&facets[location][]=OK&facets[location][]=OR&facets[location][]=PA&facets[location][]=RI&facets[location][]=SC&facets[location][]=SD&facets[location][]=TN&facets[location][]=TX&facets[location][]=UT&facets[location][]=VA&facets[location][]=VT&facets[location][]=WA&facets[location][]=WI&facets[location][]=WV&facets[location][]=WY&facets[sectorid][]=99&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000&out=json"
+    url = f"https://api.eia.gov/v2/electricity/electric-power-operational-data/data/?api_key={key}&frequency=annual&data[0]=generation&start=2021&end=2022&facets[fueltypeid][]=BIO&facets[fueltypeid][]=COW&facets[fueltypeid][]=GEO&facets[fueltypeid][]=HYC&facets[fueltypeid][]=NG&facets[fueltypeid][]=NUC&facets[fueltypeid][]=PC&facets[fueltypeid][]=PEL&facets[fueltypeid][]=SUN&facets[fueltypeid][]=WND&facets[location][]=AL&facets[location][]=AR&facets[location][]=AZ&facets[location][]=CA&facets[location][]=CO&facets[location][]=CT&facets[location][]=DC&facets[location][]=DE&facets[location][]=FL&facets[location][]=GA&facets[location][]=IA&facets[location][]=ID&facets[location][]=IL&facets[location][]=IN&facets[location][]=KS&facets[location][]=KY&facets[location][]=LA&facets[location][]=MA&facets[location][]=MD&facets[location][]=ME&facets[location][]=MI&facets[location][]=MN&facets[location][]=MO&facets[location][]=MS&facets[location][]=MT&facets[location][]=NC&facets[location][]=ND&facets[location][]=NE&facets[location][]=NH&facets[location][]=NJ&facets[location][]=NM&facets[location][]=NV&facets[location][]=NY&facets[location][]=OH&facets[location][]=OK&facets[location][]=OR&facets[location][]=PA&facets[location][]=RI&facets[location][]=SC&facets[location][]=SD&facets[location][]=TN&facets[location][]=TX&facets[location][]=UT&facets[location][]=VA&facets[location][]=VT&facets[location][]=WA&facets[location][]=WI&facets[location][]=WV&facets[location][]=WY&facets[sectorid][]=99&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000&out=json"
     response = requests.get(url).json()
     
     # dict of data is inside of another nested list 'response'
@@ -142,7 +144,8 @@ def netgen_by_sector_df(df):
 
 def netgen_by_state_df(df):
     """
-    Generates a geopandas GeoDataFrame that contains the boundaries of all US
+    Generates a geopandas GeoDataFrame that contains the boundaries of all
+    mainland US
     states read from a GeoJSON file, and fills in the dataframe with the energy 
     generation data with the energy sources added as columns.
     Returns a GeoDataFrame.
@@ -163,7 +166,7 @@ def netgen_by_state_df(df):
             if len(val.index) > 0: # checks that there's a row in subset df
                 val = val.set_index(['period']) # sets index to year so .loc can be used in next line
                 states_gdf.loc[state, fuel] = val.loc[2022, 'generation']
-    states_gdf.drop(index='Puerto Rico') # don't have energy data for puerto rico
+    states_gdf.drop(index=['Puerto Rico', 'Hawaii', 'Alaska']) # don't have energy data for puerto rico
     return states_gdf
 
 """
@@ -236,11 +239,11 @@ def netgen_by_sector_viz(key):
     ax.legend(loc=2, bbox_to_anchor=(1.05, 1.0), fontsize=10)
     # change with of ax to fit legend
     pos = ax.get_position()
-    ax.set_position([pos.x0, pos.y0, pos.width * 0.9, pos.height])
-    fig.set_figwidth(8)
+    ax.set_position([pos.x0, pos.y0, pos.width * 0.98, pos.height])
+    fig.set_figwidth(9)
     plt.tight_layout()
     
-    print('Completed Figure 2: Net electricity generation by sector in 2022')
+    print('Completed Figure 2: Net electricity generation by sector in 2022\n')
 
 def netgen_by_state_viz(key):
     """
@@ -252,12 +255,38 @@ def netgen_by_state_viz(key):
     gdf = netgen_by_state_df(get_netgen_by_state(key)) 
     print("Creating chart...")
 
+    fig, ax = plt.subplots()
+    fig.set_figwidth(10)
 
+    # creates radiobutton ax on fig
+    rax = fig.add_axes([0.04, 0.15, 0.27, 0.35])
+    fuels = list(gdf.columns[5:]) # pulls list of fuels for button labels from df
+    radio = RadioButtons(rax, labels=fuels, activecolor='maroon')
+    for circle in radio.circles:
+        circle.set_radius(0.035)
+        
+
+    gdf.plot(column='biomass', cmap = 'OrRd', scheme='QUANTILES', edgecolor = 'k', ax=ax, legend=True, legend_kwds={'loc': 'center right', 'bbox_to_anchor':(-.115,0.85)})
+    ax.set_title(f"Net generation (GWh) from biomass in the continental US")
+    plt.subplots_adjust(left = 0.35) # moves map right
+
+    def energy_source(label):
+        ''' Function performed on selection of a radio button '''
+        ax.set_title(f"Net generation (GWh) from {label} in the continental US")
+        gdf.plot(column=label, cmap = 'OrRd', scheme='QUANTILES', edgecolor = 'k', ax=ax, legend=True, legend_kwds={'loc': 'center right', 'bbox_to_anchor':(-.115,0.85)})
+
+    radio.on_clicked(energy_source)
+    plt.show()
+
+    print("Completed Figure 2: Interactive Choropleth Map\n")
+    
 
 def main():
     """ Displays visualizations. """
     key = "M75XbnCHbwIymVSeZUs2eVbnKlhd4EmWraupMZ5c"
     general_netgen_viz(key)
     netgen_by_sector_viz(key)
+    netgen_by_state_viz(key)
     plt.show()
+
 main()
